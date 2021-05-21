6e83222bee490276ee29a81f84e3940b044a25af
sprphnx <sprphnx@outlook.com>
2021-05-21 10:28:13 +0300

    angular-crash project added

---
+++ angular-crash/src/app/mock-tasks.ts
@@ -0,0 +1,8 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  { id: 1, text: 'Doctors appointment', day: 'May 5th', reminder: true },
+  { id: 2, text: 'Coach appointment', day: 'May 5th', reminder: true },
+  { id: 1, text: 'Gardner appointment', day: 'May 5th', reminder: true },
+  { id: 1, text: 'Dentist appointment', day: 'May 5th', reminder: true },
+];
